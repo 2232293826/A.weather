a0607167feaaca507c8bbb35762a70665b34ddad
2232293826 <2232293826@qq.com>
2019-08-28 17:47:31 +0800

    加入UI设置

--- app/src/main/java/com/example/a_weather/util/Utility.java
+++ app/src/main/java/com/example/a_weather/util/Utility.java
@@ -5,6 +5,8 @@ import android.text.TextUtils;
 import com.example.a_weather.db.City;
 import com.example.a_weather.db.County;
 import com.example.a_weather.db.Province;
+import com.example.a_weather.gson.Weather;
+import com.google.gson.Gson;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -74,5 +76,19 @@ public class Utility {
             }
         }
         return false;
+    }
+    /*将返回的JSON数据解析成Weather实体类*/
+    public static Weather handleWeatherResponse(String response){
+        try {
+            //通过JSONObject和JSONArray将天气数据中的主体内容解析出来
+            JSONObject jsonObject = new JSONObject(response);
+            JSONArray jsonArray = jsonObject.getJSONArray("HeWeather");
+            String weatherContent = jsonArray.getJSONObject(0).toString();
+            //将JSON数据转换成Weather对象
+            return new Gson().fromJson(weatherContent,Weather.class);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
